1ef83ebf26ccb3df011741d69798e505d9e356e0
Mihalescu Valentina <116385456+Mihalescu-Valentina@users.noreply.github.com>
2024-10-18 08:58:11 +0300

    Delete src directory

--- src/models/HostCity.tsx
+++
@@ -1,8 +0,0 @@
-export interface HostCity{
-    id: number;
-    host_city_name : string;
-    host_city_population : number;
-    host_city_mayor : string;
-    is_capital : boolean;
-    quality_factor : number;
-}
